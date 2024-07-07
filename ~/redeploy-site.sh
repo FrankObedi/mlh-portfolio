d12b3755e86f46d4bc71af70b18d025d5b54ec0d
Frank Obedi <frankmobedi@gmail.com>
2024-07-07 08:32:22 +0000

    update deploy script

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -4,6 +4,7 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 tmux new-session -s flask_app
+tmux attach-session -t flask_app
 source python3-virtualenv/bin/activate
 flask run --host=0.0.0.0
 #tmux detach-client -s flask_app
