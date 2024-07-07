a540b2baa27e667696de38d7320850f739417df0
FrankObedi <frankmobedi@gmail.com>
2024-07-07 08:58:00 +0000

    update deploy script

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -3,8 +3,4 @@ cd /root/mlh-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -s flask_app
-tmux attach-session -t flask_app
-source python3-virtualenv/bin/activate
-flask run --host=0.0.0.0
-#tmux detach-client -s flask_app
+tmux new-session -d -s flask_app "source python3-virtualenv/bin/activate; flask run --host=0.0.0.0"
