a01f02d96ac342e294149fc7b68d370ada61e4bd
FrankObedi <frankmobedi@gmail.com>
2024-07-07 05:52:04 -0400

    update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -79,6 +79,6 @@ def experience_page():
     return render_template("experience.html", title = "Experience", work_experiences=work_experiences, url=os.getenv("URL"))
 
 @app.route("/map")
-def experience_page():
+def map():
     return render_template("map.html", title = "My Travel Map", url=os.getenv("URL"))
 
