c277e9492671ad8d26297bcec73723d5cff9a1e2
FrankObedi <frankmobedi@gmail.com>
2024-07-13 02:03:32 -0400

    update redeplot script

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -4,4 +4,11 @@ tmux kill-server
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s flask_app "source python3-virtualenv/bin/activate; flask run --host=0.0.0.0"
+  
+# start tmux session
+SESSION=dev
+tmux new-session -d -s $SESSION
+tmux new-window -t $SESSION:1 -n 'flask_server'
+tmux select-window -t $SESSION:1
+tmux split-window -h
+tmux send-keys "cd /root/mlh-portfolio; source python3-virtualenv/bin/activate; flask run --host=0.0.0.0" C-m
