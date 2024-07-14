94ef2aaf560abf683950e86a06faffd6117ab179
FrankObedi <frankmobedi@gmail.com>
2024-07-14 01:00:08 -0400

    update deploy script

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 cd /root/mlh-portfolio
-tmux kill-server
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-# start tmux session
-SESSION=dev
-tmux new-session -d -s $SESSION
-tmux new-window -t $SESSION:1 -n 'flask_server'
-tmux select-window -t $SESSION:1
-tmux split-window -h
-tmux send-keys "cd /root/mlh-portfolio; source python3-virtualenv/bin/activate; flask run --host=0.0.0.0" C-m
+
+# Restart myportfolio service 
+systemctl restart myportfolio
