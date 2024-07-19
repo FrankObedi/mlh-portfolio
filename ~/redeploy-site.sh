457883d6f6aea001a1e06d75cf0145862eb95385
FrankObedi <frankmobedi@gmail.com>
2024-07-19 01:33:03 -0400

    update redeploy script

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -4,5 +4,7 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
+# renable services
+systemctl daemon-reload
 # Restart myportfolio service 
 systemctl restart myportfolio
