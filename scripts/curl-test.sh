188b6311716578a6b74d0ea42e454e064f035ff4
Frank Obedi <frankmobedi@gmail.com>
2024-07-31 11:34:30 -0400

    Delete scripts directory

--- scripts/curl-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl --request POST http://127.0.0.1:5000/api/timeline_post -d 'name=Frank&email=frankobedi6@gmail.com&content=Test the API endpoints using curl and bash script'
-curl http://127.0.0.1:5000/api/timeline_post
-curl --request DELETE http://127.0.0.1:5000/api/timeline_post/delete_all
